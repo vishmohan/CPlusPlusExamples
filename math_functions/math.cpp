430cb07598832dbfa11cf7f210ecc4ceaba65a72
Vishwa Mohan <vishwa_m@yahoo.com>
2023-02-27 08:10:17 -0600

    Added  program that uses some math functions from <cmath>  Changes to be committed: 	new file:   math_functions/Makefile         new file:   math_functions/math.cpp

---
+++ math_functions/math.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+//1. abs(x) - absolute value of x
+    int n1{5};
+    int n2{-7};
+    cout << abs(n1) << " " << abs(n2) << endl;
+
+//2. ceil(x) - smallest integer >= x
+    double n3{3.2};
+    double n4{3.6};
+    double n5{-3.2};
+    cout << ceil(n3) << " " << ceil(n4) << " " << ceil(n5) << endl;
+
+//3. floor(x) - largest integer <= x
+    cout << floor(n3) << " " << floor(n4) << " " << floor(n5) << endl;
+
+//4. sqrt(x) returns sqrt => x must be non-negative
+    double n6{47.25};
+    cout << "sqrt of " << n6 << " : " << sqrt(n6) << endl;
+
+//5. Trignometric functions: sin(x), cos(x), tan(x) note: x is in radians
+    double angle{M_PI/3} ; //angle = 60 degrees
+    cout << "Sin(PI/3) = " << sin(angle) << endl;
+    cout << "Cos(PI/3) = " << cos(angle) << endl;
+    cout << "Tan(PI/3) = " << tan(angle) << endl;
+
+//6. Logarithms: Natural log/Base 10 log
+    int n7{2};
+    cout << "Natural ln(2) = " << log(n7) << endl;
+    cout << "Base10  log10(2) = " << log10(n7) << endl;
+
+    return 0;
+}
